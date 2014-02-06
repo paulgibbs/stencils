162018c95ee8f640249d10e7d13b76cd78363adc
Paul Gibbs <paul@byotos.com>
2014-02-06 00:24:49 +0000

    VERY VERY early first pass at the stencil template parsing code

--- includes/admin/functions.php
+++ includes/admin/functions.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * When a post is saved, maybe generate a stencil template.
  *
- * @param int $post_i
+ * @param int $post_id
  */
 function dks_maybe_generate_stencil( $post_id ) {
 
@@ -40,6 +40,121 @@ function dks_maybe_generate_stencil( $post_id ) {
 	dks_generate_stencil( $post_id );
 }
 
+// http://dracoblue.net/dev/gotchas-when-parsing-xml-html-with-php/
+// This assumes post_content is definitely UTF-8
 function dks_generate_stencil( $post_id ) {
-	// can't touch this
+
+	$post = get_post( $post_id );
+	if ( ! $post )
+		return;
+
+	// Get the post's rendered HTML content.
+	$html = apply_filters( 'the_content', $post->post_content );
+	$html = str_replace( ']]>', ']]&gt;', $html );
+
+	// Strips out vertical tabs and other special UTF-8 characters to avoid errors like "non SGML character number 11".
+	$html = preg_replace('/[\x1-\x8\xB-\xC\xE-\x1F]/', '', $html );
+
+	// Make $html a complete, valid HTML document.
+	$html = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=' . esc_attr( get_option(' blog_charset' ) ) . '"/></head><body>' . $html . '</body></html>';
+
+	/**
+	 * Try to load the $html into a DOMDocument for parsing.
+	 *
+	 * We turn off libxml errors to prevent PHP throwning Notices if any problems occur when parsing the string.
+	 */
+	$node = new DOMDocument( '1.0', 'utf-8' );
+
+	libxml_use_internal_errors( true );
+	$node->loadHTML( $html );
+	$error = libxml_get_last_error();
+	libxml_use_internal_errors( false );
+
+	if ( $error ) {
+		error_log( 'stencilstodo xml parsing problem: ' . print_r( $error, true ) );
+		error_log( $html );
+		return;
+	} 
+
+	$data = convertDomNodeToDataArray($node);
+
+	// Since we faked a complete HTML document from the post_content, only the <body> part is useful.
+	$data = $data['html']['body'];	
+
+	echo '<pre>';
+	die(var_dump($node));
 }
+
+function convertDomNodeToDataArray(DomNode $node)
+    {
+        $data = array();
+        $values = array();
+        $has_value = false;
+
+        if ($node->hasChildNodes())
+        {
+            foreach ($node->childNodes as $child_node)
+            {
+                /*
+                 * A html dom node always contains one dom document type child
+                 * node with no content (DOMDocumentType#internalSubset is for
+                 * example <!DOCTYPE html>). Ignore it!
+                 */
+                if ($child_node instanceof DOMDocumentType)
+                {
+                    continue ;
+                }
+                
+                if ($child_node->nodeType === XML_TEXT_NODE)
+                {
+                    $has_value = true;
+                    $values[] = $child_node->nodeValue;
+                }
+                else
+                {
+                    $key = $child_node->nodeName;
+
+                    if (isset($data[$key]))
+                    {
+                        if (!is_array($data[$key]) || !isset($data[$key][0]))
+                        {
+                            $data[$key] = array($data[$key]);
+                        }
+                        $data[$key][] = convertDomNodeToDataArray($child_node);
+                    }
+                    else
+                    {
+                        $data[$key] = convertDomNodeToDataArray($child_node);
+                    }
+                }
+            }
+        }
+
+        if ($node->hasAttributes())
+        {
+            foreach ($node->attributes as $attribute_node)
+            {
+                $key = '@' . $attribute_node->nodeName;
+                $data[$key] = $attribute_node->nodeValue;
+            }
+        }
+
+        if ($has_value)
+        {
+            $value = implode('', $values);
+
+            if (trim($value) != '')
+            {
+                if (empty($data))
+                {
+                    $data = $value;
+                }
+                else
+                {
+                    $data['@'] = $value;
+                }
+            }
+        }
+        
+        return $data;
+    }
